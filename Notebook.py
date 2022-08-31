a500dbdf838e75282c17cc0570fc004446d20d59
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-08-31 14:01:50 +0530

    Update Notebook.py

--- Notebook.py
+++ Notebook.py
@@ -1,3 +1,4 @@
+
 # Python code to demonstrate table creation and  
 # insertions with SQL 
   
@@ -7,29 +8,36 @@ from tkinter import *
 # Part 1 Creating a simple Database
 root = Tk()
 # connecting to the database  
-connection = sqlite3.connect("myTable.db") 
+connection = sqlite3.connect("Table.db") 
   
 # cursor  
 crsr = connection.cursor() 
   
 # SQL command to create a table in the database 
-sql_command =
+sql_command = """CREATE TABLE emp (  
+staff_number INTEGER PRIMARY KEY,  
+fname text,  
+lname text,  
+gender text,  
+joining text);"""
   
 # execute the statement 
-
+crsr.execute(sql_command) 
   
 # SQL command to insert the data in the table 
-sql_command = 
+sql_command = """INSERT INTO emp VALUES (21, "Arpit", "Bansal", "M", "2014-03-28");"""
+crsr.execute(sql_command) 
   
 # another SQL command to insert the data in the table 
-sql_command = 
+sql_command = """INSERT INTO emp VALUES (1, "Bill", "Gates", "M", "1980-10-28");"""
+crsr.execute(sql_command) 
   
 # To save the changes in the files. Never skip this.  
 # If we skip this, nothing will be saved in the database. 
-
+connection.commit() 
   
 # close the connection 
-
+connection.close() 
 
 # Part 2 Creating a interface for entering values to our database.
 # Create a submit function
@@ -37,11 +45,19 @@ sql_command =
 
 def Submit():
     # connecting to the database  
-    connection = sqlite3.connect("myTable.db") 
+    connection = sqlite3.connect("Table.db") 
     # cursor  
     crsr = connection.cursor() 
     # Insert Into Table
-    
+    crsr.execute("INSERT INTO emp VALUES (:Staffno,  :f_name , :l_name , :Gender , :DOJi)",
+        {
+            'Staffno' : Staffno.get(),
+            'f_name' : f_name.get(),
+            'l_name' : l_name.get(),
+            'Gender' : Gender.get(),
+            'DOJi' : DOJi.get()
+        }
+    )
     connection.commit() # Commit all the changes to the database
     connection.close() 
     # Clear the Text Boxes
@@ -52,13 +68,13 @@ def Submit():
     DOJi.delete(0 ,END)
 def Query():
     # connecting to the database  
-    connection = sqlite3.connect("myTable.db") 
+    connection = sqlite3.connect("Table.db") 
     # cursor  
     crsr = connection.cursor() 
     # Insert Into Table
-    
+    crsr.execute("SELECT *,oid FROM emp")  
     # store all the fetched data in the ans variable 
-    
+    ans = crsr.fetchall()  
     # Since we have already selected all the data entries  
     # using the "SELECT *" SQL command and stored them in  
     # the ans variable, all we need to do now is to print  
@@ -66,28 +82,50 @@ def Query():
     # As we want to display the dataframe in our GUI we can use label widget
     # ans is a list and for example we want to find the names of all the 
     # People in datatable we can use the following command
-    
+    records =""
+    for record in ans:
+        records += record[1] +" " +record[2] +"   " + str(record[0]) + '\n'
+    Label(root , text = records ,bg = "yellow").grid(row = 10 ,column = 0 , columnspan = 2 )
 def delete():
     # connecting to the database  
-    connection = sqlite3.connect("myTable.db") 
+    connection = sqlite3.connect("Table.db") 
     # cursor  
     crsr = connection.cursor() 
     # oid is the Primary field
     # We can choose any column instead of oid
-    
-   
-    
+    connection.execute("DELETE from emp where oid=" + deli.get())
+    deli.delete(0 , END) 
+    connection.commit() 
+    connection.close()
 def edit():
-    connection = sqlite3.connect("myTable.db") 
+    connection = sqlite3.connect("Table.db") 
     # cursor  
     crsr = connection.cursor() 
-    
+    crsr.execute("""UPDATE emp SET
+    staff_number = :staffnum,
+    fname = :f_name,
+    lname = :l_name,
+    gender = :Gender,
+    joining = :DOJi
+    Where oid = :Aid""",
+        {
+            'staffnum' : Staffno_edit.get(),
+            'f_name' : f_name_edit.get(),
+            'l_name' : l_name_edit.get(),
+            'Gender' : Gender_edit.get(),
+            'DOJi' : DOJi_edit.get(),
+            'Aid' : deli.get()
+        }
+    )
+    connection.commit() 
+    connection.close()
 def update():
     up = Tk() 
-    connection = sqlite3.connect("myTable.db") 
+    connection = sqlite3.connect("Table.db") 
     crsr = connection.cursor() 
     record_id = deli.get()
-   
+    crsr.execute("SELECT * FROM emp WHERE oid = " + record_id)
+    records = crsr.fetchall()
     # Create Text Boxes
     global Staffno_edit
     Staffno_edit = Entry(up , width = 30)
@@ -121,7 +159,12 @@ def update():
     DOJi_label = Label(up, text = "Date of Joining") 
     DOJi_label.grid(row = 4 , column =0)
      # Loop through results and complete all the fields
-    
+    for record in records:
+        Staffno_edit.insert(0, record[0])
+        f_name_edit.insert(0, record[1])
+        l_name_edit.insert(0,record[2])
+        Gender_edit.insert(0, record[3])
+        DOJi_edit.insert(0, record[4])
     # Button for Submit
     Button(up , text = "Submit Changes" , command = edit).grid(row = 5 , column = 1,pady = 10 , padx = 10)
     # Button for exit
